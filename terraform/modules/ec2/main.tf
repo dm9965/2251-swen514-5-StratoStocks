78af7a07f66ae14a3855e23e99ac93d0396c03fe
domenicmangano <dm9965@rit.edu>
2025-10-21 20:52:57 -0400

    Remvoed tags from ec2

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -4,8 +4,4 @@ resource "aws_instance" "stratostocks_ec2" {
   subnet_id              = var.subnet_id  # Place in the public subnet just pick the first one
   vpc_security_group_ids = var.sg_ids  # Attach the EC2 security group
   key_name               = var.key_name # Replace with your SSH key pair name
-
-  tags = {
-    Name = "StratoStocks EC2 Instance"
-  }
 }
